4e30c77bd0409599b58ae648be510e0dc93a7775
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-28 19:27:16 +0200

    Section 2: Conversión de variables

---
+++ Section02/15.js
@@ -0,0 +1,7 @@
+var primerNombre, edad, sexo, esSoltero;
+primerNombre = 'Juan Antonio';
+edad = 34
+sexo = 'M'
+esSoltero = false;
+
+console.log('El nombre es: '+primerNombre+', y su edad es:'+edad+' ¿es soltero? '+false);
