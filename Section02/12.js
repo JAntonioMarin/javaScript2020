a3fa200edeaf4df95a030d69debf00d4b8b2b6b5
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-28 19:12:28 +0200

    Section 2: Variables y tipos de datos

---
+++ Section02/12.js
@@ -0,0 +1,20 @@
+var primerNombre = 'Juan Antonio';
+console.log(primerNombre);
+
+var edad = 34;
+var sueldo = 1800.99
+
+console.log(edad);
+console.log(sueldo);
+
+var tieneTrabajo = true;
+
+console.log(tieneTrabajo);
+
+var puestoDeTrabajo;
+
+console.log(puestoDeTrabajo);
+
+tieneTrabajo = null;
+
+console.log(tieneTrabajo);
