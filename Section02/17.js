cc847d1985e85a210c959be5d130dd9c295cbf0d
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-30 01:11:34 +0200

    Section 2: Operadores matemáticos

---
+++ Section02/17.js
@@ -0,0 +1,15 @@
+// Operadores matematicos (+,-,*,/)
+
+var edadJuan, edadAntonio;
+
+edadJuan = 34;
+edadAntonio = 28;
+
+var diferenciaEdad = edadJuan - edadAntonio;
+
+console.log(diferenciaEdad);
+
+var sumaEdades = edadJuan + edadAntonio;
+
+console.log(sumaEdades);
+
