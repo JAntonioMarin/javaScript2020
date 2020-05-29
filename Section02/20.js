630c18cdb391ea9c7763a51dc55a380e5b76a686
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-30 01:30:19 +0200

    Section 2: Operadores de incremento y decremento

---
+++ Section02/20.js
@@ -0,0 +1,14 @@
+// Operadores de incremento (unarios aritméticos)
+
+var numero = 7;
+numero ++;
+console.log(numero);
+
+numero --;
+console.log(numero);
+
+numero += 7;
+console.log(numero);
+
+numero -= 2;
+console.log(numero);
