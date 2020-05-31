00ad70ed1b8356fe36f0eca394b2ec39e29d4117
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-31 19:11:30 +0200

    Section 5: Ejemplo funcion convierte Fahrenheit a Celsius

---
+++ Section05/46.js
@@ -0,0 +1,8 @@
+// Ejemplo funcion convierte Fahrenheit a Celsius
+
+function convertirFACelsius(gradosFA){
+    return ((gradosFA-32)*5/9);
+}
+
+console.log(convertirFACelsius(32));
+console.log(convertirFACelsius(68));
