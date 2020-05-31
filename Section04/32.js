209b6f487cda0cb6a362505bfcb0b7536727ed27
JAntonioMarin <avalanch.psp@gmail.com>
2020-05-31 16:33:51 +0200

    Section4: Sentencia if/else

---
+++ Section04/32.js
@@ -0,0 +1,18 @@
+var nombre = 'Juan';
+var estadoCivil = 'soltero';
+var estaCasado = false;
+
+
+// === tiene que ser igual y del mismo tipo
+if (estadoCivil === 'casado'){
+    console.log(nombre+' está casado');
+}else{
+    console.log(nombre + ' esta soltero');
+}
+
+
+if (estaCasado){
+    console.log(nombre+' está casado');
+}else{
+    console.log(nombre + ' esta soltero');
+}
