be9bf12d7e7f5fcd57c852d5bf72dc98f26534e1
JAntonioMarin <avalanch.psp@gmail.com>
2020-06-01 20:05:55 +0200

    Section 5: Ejercicio de codificacion 3

---
+++ Section05/48.js
@@ -0,0 +1,7 @@
+// Ejercicio de codificacion
+
+function aniosJubilacion(anioNacimiento, nombre){
+    console.log(nombre+' te faltan '+(65-(2020-anioNacimiento)+' años para tu jubilación.'));
+}
+
+aniosJubilacion(1985, 'Juan');
