100062855ff08a0adaff20e3ead300ccd0dbacfa
JAntonioMarin <avalanch.psp@gmail.com>
2020-06-05 19:30:39 +0200

    Section 6: Declaracion de arrays en JavaScript

---
+++ Section06/60.js
@@ -0,0 +1,15 @@
+// Declaracion de arrays en Javascript
+
+var nombre = ['Juan', 'Pepe', 'Ana', 'Teresa'];
+var vegetales = new Array('Tomate', 'Lechuga', 'Zanahoria');
+
+console.log(nombre[1]);
+console.log(vegetales[2]);
+
+nombre[1] = 'Jose';
+vegetales[2] = 'Nabo';
+
+console.log(nombre[1]);
+console.log(vegetales[2]);
+
+console.log(nombre.length);
