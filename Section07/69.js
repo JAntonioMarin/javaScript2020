7c8b1b572ec0f46e66bad6aef2941b3b9525ceed
JAntonioMarin <avalanch.psp@gmail.com>
2020-06-07 00:27:24 +0200

    Section 7: Examinando el DOM

---
+++ Section07/69.js
@@ -0,0 +1,12 @@
+// Examinando el DOM
+
+console.dir(document); //Todo el documento en consola
+
+console.log(document.title);
+document.title = 'Prueba';
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.all[5]);    ;
+console.log(document.forms);
+console.log(document.images);
