48220420a0734f19485b05e242f73c1a127351f3
JAntonioMarin <avalanch.psp@gmail.com>
2020-06-06 18:27:22 +0200

    Section 6: Creando objetos con la sintaxis Object

---
+++ Section06/64.js
@@ -0,0 +1,8 @@
+// Creando objetos con la sintaxis Object
+
+var persona2 = new Object();
+persona2.nombre = 'Ana';
+persona2.apellido = 'Pinedo';
+persona2['trabajo'] = 'WebDeveloper';
+
+console.log(persona2);
