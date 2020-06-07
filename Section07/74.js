7bff68a886d29c51ad32e477d16a8cccd64b178b
JAntonioMarin <avalanch.psp@gmail.com>
2020-06-07 11:22:38 +0200

    Section 7: Elementos y nodos hermanos

---
+++ Section07/74.js
@@ -0,0 +1,16 @@
+// Elementos y nodos hermanos
+
+var itemsList = document.querySelector('#items');
+
+//previousSibling
+console.log(itemsList.previousSibling);
+
+//previousElementSibling
+console.log(itemsList.previousElementSibling);
+
+//nextSibling
+console.log(itemsList.nextSibling);
+
+//nextElementSibling
+console.log(itemsList.nextElementSibling);
+
