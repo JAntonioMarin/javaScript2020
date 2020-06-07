bbafe5c740c7e4587846f47e25a7a64562e9b65f
JAntonioMarin <avalanch.psp@gmail.com>
2020-06-07 11:17:31 +0200

    Section 7: Elementos y nodos hijos

---
+++ Section07/73.js
@@ -0,0 +1,20 @@
+// Elementos y nodos hijos
+
+var itemsList = document.querySelector('#items');
+//childNodes
+console.log(itemsList.childNodes);
+
+
+//children
+console.log(itemsList.children);
+
+
+//firstChild
+console.log(itemsList.firstChild);
+console.log(itemsList.firstElementChild);
+
+
+
+//lastChild
+console.log(itemsList.lastChild);
+console.log(itemsList.lastElementChild);
