44c7d86727ea9b90e15ff2291b38456403bf5c87
JAntonioMarin <avalanch.psp@gmail.com>
2020-07-06 20:43:30 +0200

    Section 7: Buscar elementos en una lista

--- Section07/78.js
+++ Section07/78.js
@@ -3,7 +3,10 @@
 var form = document.getElementById('formAgregar');
 var lista = document.getElementById('items');
 
+//Evento submit del formulario
 form.addEventListener('submit', agregarItem);
+//Evento click de la lista
+lista.addEventListener('click', eliminarItem);
 
 function agregarItem(e){
     e.preventDefault();
@@ -14,10 +17,19 @@ function agregarItem(e){
     li.appendChild(document.createTextNode(newItem));
 
     var botonDel = document.createElement('button');
-    botonDel.className = 'btn btn-danger btn-sm float-right';
+    botonDel.className = 'btn btn-danger btn-sm float-right eliminar';
     botonDel.appendChild(document.createTextNode('X'));
 
     li.appendChild(botonDel);
 
     lista.appendChild(li);
+}
+
+function eliminarItem(e){
+    if(e.target.classList.contains('eliminar')){
+        if(confirm('¿Seguro que desea eliminar el elemento?')){
+            var li = e.target.parentElement;
+            lista.removeChild(li);
+        }        
+    }
 }
