58ca5efcffdf20dc9aa6526b693931ba9e87072f
JAntonioMarin <avalanch.psp@gmail.com>
2020-07-08 20:03:30 +0200

    Section 8: Bloque y alcance de las variables

---
+++ Section08/app.js
@@ -0,0 +1,13 @@
+//ES5 - Variables 
+var nombre = 'Pablo';
+var edad = 35;
+nombre = 'Carlos';
+console.log(nombre);
+
+//ES6 - Variables 
+const nombre6 = 'Pedro';
+let edad6 = 28;
+//nombre6 = 'Ana'; //No se puede, da error
+edad6 = 29;
+console.log(nombre6);
+console.log(edad6);
