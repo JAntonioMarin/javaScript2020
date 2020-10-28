9e784344c33571f05c6b83b0be4d592491ca050f
JAntonioMarin <avalanch.psp@gmail.com>
2020-10-28 19:38:58 +0100

    Section 8: Operador de propagacion (spread operator)

--- Section08/app.js
+++ Section08/app.js
@@ -165,3 +165,22 @@ datos.forEach((value,key)=>{
     console.log(`${key} : ${value}`);
 })
 
+// Operador de propagacion (spread operator)
+
+var suma = function(a,b,c,d){
+    return a+ b+ c + d;
+}
+
+var ope1 = suma(10,20,30,40);
+console.log(ope1);
+
+var valores = [10,20,30,40];
+const oper2 = suma(...valores);
+console.log(oper2);
+
+const rrhh = ['Pedro', 'Pepe', 'Luis'];
+const contabilidad = ['Felipe', 'Carlos', 'Maria'];
+const empresa = [...rrhh, ...contabilidad ];
+
+console.log(empresa);
+
