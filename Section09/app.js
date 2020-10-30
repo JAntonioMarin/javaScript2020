76eb8e244c280dbd7f25bc3d9f1a6eda22a50b43
JAntonioMarin <avalanch.psp@gmail.com>
2020-10-30 19:14:58 +0100

    Section 09: Ejercicio de codificacion Objetos

--- Section09/app.js
+++ Section09/app.js
@@ -1,3 +1,4 @@
+// Objetos - Lo básico
 let miLibro = {
     titulo: 'El libro de JavaScript',
     autor: 'Grover Pablo Vásquez',
@@ -9,3 +10,15 @@ console.log(miLibro.titulo);
 console.log(`${miLibro.titulo} creado por ${miLibro.autor}`);
 miLibro.paginas = 500;
 console.log(miLibro.paginas);
+
+// Ejecicio de codificacion Objetos
+// nombre, edad, ciudad
+// cambiar la edad
+// Mostrar datos de la persona
+let persona = {
+    nombre: 'Juan Antonio',
+    edad: 34,
+    ciudad: 'Granada'
+}
+persona.edad = 35;
+console.log(`${persona.nombre} tiene ${persona.edad} años y vive en ${persona.ciudad}`);
