4f7fd7f39d1f26e1c9cc8508f0d37d27760614c2
JAntonioMarin <avalanch.psp@gmail.com>
2020-11-26 19:20:57 +0100

    Section 10: Clases, Sintaxis y Contructores- Parte 2

--- Section10/app.js
+++ Section10/app.js
@@ -1,12 +1,22 @@
 ////// Manejo de clases en JavaScript //////
 
 class Persona{
-    constructor(nombre ,edad){
+    constructor(nombre ,edad, trabajos = []){
         this.nombre = nombre;
         this.edad = edad;
+        this.trabajos = trabajos;
+    }
+
+    getBiografia(){
+        let biografia = `${this.nombre}, tiene ${this.edad}, Trabajo/s: `;
+
+        this.trabajos.forEach((trabajo) => {
+            biografia += `${trabajo},`;
+        })
+        return biografia;
     }
 }
-const persona1 = new Persona('Pablo', 30);
-const persona2 = new Persona('Ana', 25);
-console.log(persona1.nombre);
-console.log(persona2.nombre);
+const persona1 = new Persona('Pablo', 30, ['Arquitecto', 'Doctor']);
+const persona2 = new Persona('Ana', 25, ['Ingeniera', 'Profesora']);
+console.log(persona1.getBiografia());
+console.log(persona2.getBiografia());
