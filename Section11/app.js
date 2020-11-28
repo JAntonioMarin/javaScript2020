41ce1d1dc8fcea16fe4a29ba988598e1c835c638
JAntonioMarin <avalanch.psp@gmail.com>
2020-11-28 17:50:54 +0100

    Section 11:Agregar/quitar elemento al inicio/final

--- Section11/app.js
+++ Section11/app.js
@@ -8,3 +8,15 @@ console.log(ceviche.length);
 ceviche[0] = 'papas';
 console.log(ceviche);
 console.log(ceviche[2]);
+
+// Agregar/quitar elemento al inicio/final
+//Al final
+ceviche.push('limón');
+console.log(ceviche);
+ceviche.pop();
+console.log(ceviche);
+//Al inicio
+ceviche.unshift('pimiento');
+console.log(ceviche);
+ceviche.shift();
+console.log(ceviche);
