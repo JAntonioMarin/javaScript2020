1c257fef50f96c5db0a43803abe2bc2a22a6b644
JAntonioMarin <avalanch.psp@gmail.com>
2020-11-28 17:46:59 +0100

    Section11: Arrays básicos

---
+++ Section11/app.js
@@ -0,0 +1,10 @@
+/// Arraus en ES6+///  
+
+const ceviche = ['pescado', 'mariscos', 'cebolla'];
+
+console.log(ceviche);
+console.log(ceviche.length);
+
+ceviche[0] = 'papas';
+console.log(ceviche);
+console.log(ceviche[2]);
